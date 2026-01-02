9c6a8f40d72d5ec28e7a7bd09b5b2754b3c7a6ac
Aman Raj <amanr11314@gmail.com>
2026-01-03 00:54:17 +0530

    solved day1

---
+++ day1.py
@@ -0,0 +1,33 @@
+"""
+11=>R8 => (11+8)%100 = 19 => L19 => (19-19)%100 => 0
+5=>L10 => (5-10+100)%100 = 95 => R5 => (95+5)%100 => 0
+
+Number of times dial is left pointing to 0 after any rotation in squence
+50
+L86 => 50-68+100 %100 => 82
+L30 => 82-30 %100 => 52
+R48 => 52+48 %100 => 0
+L5 => 0-5+100 %100 =>95
+"""
+
+def solve():
+    ans = 0
+    start = 50
+    with open("input.txt", "r") as file:
+        for line in file:
+            direction, rotation = line[0], int(line[1:])
+            if direction == 'L':
+                start = (start - rotation + 100)%100
+            elif direction == 'R':
+                start = (start + rotation + 100)%100
+            else:
+                pass
+            
+            if start==0:
+                ans += 1
+    
+    return ans
+
+if __name__=="__main__":
+    password = solve()
+    print(password)
