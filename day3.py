9629d937d4dd07800ba1335f82bf7cbf4344c06a
Aman Raj <amanr11314@gmail.com>
2026-01-05 23:35:14 +0530

    day3 part 2 done

--- day3.py
+++ day3.py
@@ -17,8 +17,33 @@ def solve():
             ans += maximum_joltage
     return ans
 
+def solve2():
+    ans = 0
+    n = 12
+    
+    def maxmimum_subsequence_greedy(s: str, n:int):
+        drop = len(s)-n
+        st = []
+        for d in s:
+            # pop stack digit if curr digit > stack.top() and can_drop(drop>0)
+            while drop and st and st[-1] < d:
+                st.pop()
+                drop -= 1
+            st.append(d)
+        
+        return int("".join(st[:n]))
+    
+    with open("input.txt", "r") as file:
+        for line in file:
+            batteries = line.strip()
+            curr = maxmimum_subsequence_greedy(batteries, n)
+            ans += curr
+
+    return ans
+
 if __name__=="__main__":
-    total_output_joltage = solve()
+    # total_output_joltage = solve()
+    total_output_joltage = solve2()
     print(total_output_joltage)
     
 
